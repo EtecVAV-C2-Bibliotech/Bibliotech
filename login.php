1712d62879533ec03864468970f623d311048ee4
EtecVAV-C2-Bibliotech <analfabetos.etec@gmail.com>
2025-09-12 08:18:10 -0300

    Teste inicial do Bootstrap

--- login.php
+++ login.php
@@ -47,10 +47,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <!DOCTYPE html>
 <html>
 <head>
+
     <title>Login</title>
+      
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/estilo.css">
 </head>
-<body>
+<body class="bg-light d-flex align-items-center justify-content-center vh-100">
     <div class="container">
         <h2>Login</h2>
         <?php if ($erro): ?>
